d5bfcdf4b4036e172ba6d8a94005f2ad188e6d93
Eber <52986565+doneber@users.noreply.github.com>
2022-05-03 18:09:12 -0400

    Fix darkmode default

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 import './style.css'
 
-let darkMode = true
-document.getElementById('darkModeBtn').addEventListener('click', function changeTheme() {
+let darkMode = false
+function changeTheme() {
     if (darkMode)
         document.getElementById('darkModeBtn').innerText = 'Dark mode'
     else
         document.getElementById('darkModeBtn').innerText = 'Light mode'
     document.body.classList.toggle('dark-theme')
     darkMode = !darkMode
-})
+}
+document.getElementById('darkModeBtn').addEventListener('click', changeTheme)
+changeTheme() // starts with darkMode
