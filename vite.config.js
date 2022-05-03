02bbce02ba28a1412979c1d943c3742eaff64041
Eber <52986565+doneber@users.noreply.github.com>
2022-05-03 17:49:07 -0400

    Add html plugin (minifier)

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from "vite";
 import postcssNesting from 'postcss-nesting';
+import { createHtmlPlugin } from 'vite-plugin-html'
 
 export default defineConfig({
+    plugins: [
+        createHtmlPlugin({
+            minify: true,
+        })
+    ],
     css: {
         postcss: {
             plugins: [
