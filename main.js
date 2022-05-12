cf6a74be64623ccf3219426ce670028cbad18ae6
Eber <52986565+doneber@users.noreply.github.com>
2022-05-12 11:59:16 -0400

    Add underline items scrolling (css)

--- main.js
+++ main.js
@@ -5,3 +5,27 @@ function changeTheme() {
 }
 
 document.querySelector('#themeOption').addEventListener('click', changeTheme)
+const [home, projects, about] = [
+    document.querySelector('#home'),
+    document.querySelector('#projects'),
+    document.querySelector('#contact'),
+]
+const navAncleItems = [...document.querySelectorAll('body header nav ul li')].map(el => el.firstChild)
+const calcOffsetTop = (el) => el.getBoundingClientRect().top
+window.onscroll = function () {
+    if (calcOffsetTop(home) <= 105) {
+        navAncleItems[2].classList.remove('underline')
+        navAncleItems[3].classList.remove('underline')
+        navAncleItems[1].classList.add('underline')
+    }
+    if (calcOffsetTop(projects) <= 105) {
+        navAncleItems[2].classList.add('underline')
+        navAncleItems[1].classList.remove('underline')
+        navAncleItems[3].classList.remove('underline')
+    }
+    if (calcOffsetTop(about) <= 105) {
+        navAncleItems[3].classList.add('underline')
+        navAncleItems[1].classList.remove('underline')
+        navAncleItems[2].classList.remove('underline')
+    }
+}
