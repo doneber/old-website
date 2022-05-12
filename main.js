86058cc31eacbc2e830aa8242ec9e3db7a3f45cb
Eber <52986565+doneber@users.noreply.github.com>
2022-05-12 14:41:47 -0400

    Update styles and scroll config js

--- main.js
+++ main.js
@@ -12,17 +12,17 @@ const [home, projects, about] = [
 const navAncleItems = [...document.querySelectorAll('body header nav ul li')].map(el => el.firstChild)
 const calcOffsetTop = (el) => el.getBoundingClientRect().top
 window.onscroll = function () {
-    if (calcOffsetTop(home) <= 105) {
+    if (calcOffsetTop(home) <= 60) {
         navAncleItems[2].classList.remove('underline')
         navAncleItems[3].classList.remove('underline')
         navAncleItems[1].classList.add('underline')
     }
-    if (calcOffsetTop(projects) <= 105) {
+    if (calcOffsetTop(projects) <= 60) {
         navAncleItems[2].classList.add('underline')
         navAncleItems[1].classList.remove('underline')
         navAncleItems[3].classList.remove('underline')
     }
-    if (calcOffsetTop(about) <= 105) {
+    if (calcOffsetTop(about) <= 60) {
         navAncleItems[3].classList.add('underline')
         navAncleItems[1].classList.remove('underline')
         navAncleItems[2].classList.remove('underline')
