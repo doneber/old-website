a9bb4239c649b1fc2e42aa2a88044d498deb6db5
Eber <52986565+doneber@users.noreply.github.com>
2022-05-14 12:35:49 -0400

    Add linter standard & fix linter errors

--- main.js
+++ main.js
@@ -1,30 +1,30 @@
 import './style.css'
 
 document.querySelector('#themeOption').addEventListener('click', () => {
-    document.body.classList.toggle('dark-theme')
+  document.body.classList.toggle('dark-theme')
 })
 
 const [home, projects, about] = [
-    document.querySelector('#home'),
-    document.querySelector('#projects'),
-    document.querySelector('#contact'),
+  document.querySelector('#home'),
+  document.querySelector('#projects'),
+  document.querySelector('#contact')
 ]
 const navAncleItems = [...document.querySelectorAll('body header nav ul li')].map(el => el.firstChild)
 const calcOffsetTop = (el) => el.getBoundingClientRect().top
 window.onscroll = function () {
-    if (calcOffsetTop(home) <= 60) {
-        navAncleItems[2].classList.remove('underline')
-        navAncleItems[3].classList.remove('underline')
-        navAncleItems[1].classList.add('underline')
-    }
-    if (calcOffsetTop(projects) <= 60) {
-        navAncleItems[2].classList.add('underline')
-        navAncleItems[1].classList.remove('underline')
-        navAncleItems[3].classList.remove('underline')
-    }
-    if (calcOffsetTop(about) <= 60) {
-        navAncleItems[3].classList.add('underline')
-        navAncleItems[1].classList.remove('underline')
-        navAncleItems[2].classList.remove('underline')
-    }
+  if (calcOffsetTop(home) <= 60) {
+    navAncleItems[2].classList.remove('underline')
+    navAncleItems[3].classList.remove('underline')
+    navAncleItems[1].classList.add('underline')
+  }
+  if (calcOffsetTop(projects) <= 60) {
+    navAncleItems[2].classList.add('underline')
+    navAncleItems[1].classList.remove('underline')
+    navAncleItems[3].classList.remove('underline')
+  }
+  if (calcOffsetTop(about) <= 60) {
+    navAncleItems[3].classList.add('underline')
+    navAncleItems[1].classList.remove('underline')
+    navAncleItems[2].classList.remove('underline')
+  }
 }
--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,18 @@
-import { defineConfig } from "vite";
-import postcssNesting from 'postcss-nesting';
+import { defineConfig } from 'vite'
+import postcssNesting from 'postcss-nesting'
 import { createHtmlPlugin } from 'vite-plugin-html'
 
 export default defineConfig({
-    plugins: [
-        createHtmlPlugin({
-            minify: true,
-        })
-    ],
-    css: {
-        postcss: {
-            plugins: [
-                postcssNesting
-            ],
-        },
-    },
-});
+  plugins: [
+    createHtmlPlugin({
+      minify: true
+    })
+  ],
+  css: {
+    postcss: {
+      plugins: [
+        postcssNesting
+      ]
+    }
+  }
+})
